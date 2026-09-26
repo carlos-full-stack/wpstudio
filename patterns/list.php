8525fd9e6e45718898f83fc9eeb505e2b922301a
Carlos <carlosgilino@gmail.com>
2026-09-26 11:34:29 +0200

    add steps pattern

---
+++ patterns/list.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Title: Lista
+ * Slug: wpstudio/list
+ * Description: Lista de beneficios con icono, título y descripción. Editable: iconos, títulos y textos.
+ */
+?>
+
+<!-- wp:group {"metadata":{"patternName":"wpstudio/list","name":"Lista de beneficios"},"align":"full","className":"wp-list","style":{"spacing":{"blockGap":"var:preset|spacing|medium","padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull wp-list" style="padding-right:0;padding-left:0"><!-- wp:group {"className":"wp-benefit-item","style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+    <div class="wp-block-group wp-benefit-item"><!-- wp:html -->
+        <svg width="42" height="43" viewBox="0 0 42 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 10.0978L21 19.8414V41.038L1 32.491V10.0978Z" fill="#94A3B8" />
+            <path d="M21 1.09784V19.9013M21 41.0978L1 32.5508V10.1577L21 1.09784L41 10.1577V32.5508L21 41.0978ZM21 19.9013L1 10.1577M21 19.9013L41 10.1577M21 19.9013V41.0978" stroke="black" stroke-width="2" />
+        </svg>
+        <!-- /wp:html -->
+
+        <!-- wp:group {"layout":{"type":"constrained"}} -->
+        <div class="wp-block-group"><!-- wp:heading {"level":3} -->
+            <h3 class="wp-block-heading">Lorem ipsum dolor sit amet</h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph -->
+            <p>Consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"className":"wp-benefit-item","style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+    <div class="wp-block-group wp-benefit-item"><!-- wp:html -->
+        <svg width="42" height="43" viewBox="0 0 42 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M40 10.0978L21 19.8603V41.0978L40 32.5343V10.0978Z" fill="#94A3B8" />
+            <path d="M21 1.09784V19.9013M21 41.0978L1 32.5508V10.1577L21 1.09784L41 10.1577V32.5508L21 41.0978ZM21 19.9013L1 10.1577M21 19.9013L41 10.1577M21 19.9013V41.0978" stroke="black" stroke-width="2" />
+        </svg>
+        <!-- /wp:html -->
+
+        <!-- wp:group {"layout":{"type":"constrained"}} -->
+        <div class="wp-block-group"><!-- wp:heading {"level":3} -->
+            <h3 class="wp-block-heading">Suspendisse potenti nullam ac tortor</h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph -->
+            <p>Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"className":"wp-benefit-item","style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+    <div class="wp-block-group wp-benefit-item"><!-- wp:html -->
+        <svg width="42" height="43" viewBox="0 0 42 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M21 1.09784V19.9013M21 41.0978L1 32.5508V10.1577L21 1.09784L41 10.1577V32.5508L21 41.0978ZM21 19.9013L1 10.1577M21 19.9013L41 10.1577M21 19.9013V41.0978" stroke="black" stroke-width="2" />
+            <path d="M21 19.0978L3 10.355L21 2.09784L39 10.355L21 19.0978Z" fill="#94A3B8" />
+        </svg>
+        <!-- /wp:html -->
+
+        <!-- wp:group {"layout":{"type":"constrained"}} -->
+        <div class="wp-block-group"><!-- wp:heading {"level":3} -->
+            <h3 class="wp-block-heading">Curabitur pretium tincidunt lacus</h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph -->
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
